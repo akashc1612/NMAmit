4c0c96cc6c7487b88da2ab15b3d7a9ac2e5523c0
akash singh chauhan <58021571+ras733171@users.noreply.github.com>
2020-08-27 15:50:50 +0530

    Create nmamit.py

---
+++ nmamit.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import *
+import os
+def fun():
+    print("WELCOME TO NMAMIT:")
+    print("1. NMAMIT official site")
+    print("2. Moodle")
+    print("3. Parents Login")
+    a = input("Please enter one of the above options.")
+    try:
+        if int(a) == 1:
+            driver = webdriver.Chrome(executable_path="C:/Users/ras73/Desktop/chromedriver_win32/chromedriver.exe")
+            driver.get("https://www.nmamit.nitte.edu.in/")
+        elif int(a) == 2:
+            driver = webdriver.Chrome(executable_path="C:/Users/ras73/Desktop/chromedriver_win32/chromedriver.exe")
+            driver.get("http://guru.nmamit.in/login/index.php")
+        elif int(a) == 3:
+            driver = webdriver.Chrome(executable_path="C:/Users/ras73/Desktop/chromedriver_win32/chromedriver.exe")
+            driver.get("http://www.ioncudos.com/nmamit_parent/login")
+        else:
+            clear = lambda: os.system('cls')
+            clear()
+            print("please enter a correct option")
+            fun()
+    except:
+        clear = lambda: os.system('cls')
+        clear()
+        print("please enter a correct option")
+        fun()
+fun()
